e2931421d953c3251d2d4f4c8ccf4b4174b4ac72
Darya Shashko <darya_shashko@epam.com>
2025-07-30 12:17:01 +0200

    feat(1): add task module

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,81 @@
+import {
+  Controller,
+  Get,
+  Param,
+  NotFoundException,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  BadRequestException,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
+import { CreateTaskDto } from './create-task.dto';
+import { FindOneParams } from './find-one.params';
+import { UpdateTaskStatusDto } from './update-task-status.dto';
+import { UpdateTaskDto } from './update-task.dto';
+import { WrongTaskStatusException } from './exceptions/wrong-task-status-exception';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
+  }
+
+  @Get('/:id')
+  public findOne(@Param() params: FindOneParams): ITask {
+    return this.findOneOrFail(params.id);
+  }
+
+  @Post()
+  public createTask(@Body() data: CreateTaskDto): ITask {
+    return this.tasksService.create(data);
+  }
+
+  @Patch('/:id/status')
+  public updateTaskStatus(
+    @Param() params: FindOneParams,
+    @Body() body: UpdateTaskStatusDto,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+    task.status = body.status;
+    return task;
+  }
+
+  @Patch('/:id')
+  public updateTask(
+    @Param() params: FindOneParams,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+    try {
+      return this.tasksService.updateTask(task, updateTaskDto);
+    } catch (error) {
+      if (error instanceof WrongTaskStatusException) {
+        throw new BadRequestException(error.message);
+      }
+      throw error;
+    }
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public deleteTask(@Param() params: FindOneParams): void {
+    const task = this.findOneOrFail(params.id);
+    this.tasksService.deleteTask(task);
+  }
+
+  private findOneOrFail(id: string): ITask {
+    const task = this.tasksService.findOne(id);
+    if (task) {
+      return task;
+    }
+    throw new NotFoundException(`Task with id ${id} not found`);
+  }
+}
